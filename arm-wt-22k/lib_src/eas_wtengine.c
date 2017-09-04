4ad494fb763c9a4ce176a90ee9c93a7b47ac5ec6
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-09-04 17:39:45 +0000

    Fix cooper cm-11.0 build
    Change-Id: Ie993841afd25aa987116a4f7f8a8a60ae125abd9

--- arm-wt-22k/lib_src/eas_wtengine.c
+++ arm-wt-22k/lib_src/eas_wtengine.c
@@ -93,7 +93,7 @@ void WT_VoiceGain (S_WT_VOICE *pWTVoice, S_WT_INT_FRAME *pWTIntFrame)
     numSamples = pWTIntFrame->numSamples;
     if (numSamples <= 0) {
         ALOGE("b/26366256");
-        android_errorWriteLog(0x534e4554, "26366256");
+        //android_errorWriteLog(0x534e4554, "26366256");
         return;
     }
     pMixBuffer = pWTIntFrame->pMixBuffer;
@@ -192,7 +192,7 @@ void WT_Interpolate (S_WT_VOICE *pWTVoice, S_WT_INT_FRAME *pWTIntFrame)
     numSamples = pWTIntFrame->numSamples;
     if (numSamples <= 0) {
         ALOGE("b/26366256");
-        android_errorWriteLog(0x534e4554, "26366256");
+        //android_errorWriteLog(0x534e4554, "26366256");
         return;
     }
     pOutputBuffer = pWTIntFrame->pAudioBuffer;
@@ -290,7 +290,7 @@ void WT_InterpolateNoLoop (S_WT_VOICE *pWTVoice, S_WT_INT_FRAME *pWTIntFrame)
     numSamples = pWTIntFrame->numSamples;
     if (numSamples <= 0) {
         ALOGE("b/26366256");
-        android_errorWriteLog(0x534e4554, "26366256");
+        //android_errorWriteLog(0x534e4554, "26366256");
         return;
     }
     pOutputBuffer = pWTIntFrame->pAudioBuffer;
@@ -383,7 +383,7 @@ void WT_VoiceFilter (S_FILTER_CONTROL *pFilter, S_WT_INT_FRAME *pWTIntFrame)
     numSamples = pWTIntFrame->numSamples;
     if (numSamples <= 0) {
         ALOGE("b/26366256");
-        android_errorWriteLog(0x534e4554, "26366256");
+        //android_errorWriteLog(0x534e4554, "26366256");
         return;
     }
     pAudioBuffer = pWTIntFrame->pAudioBuffer;
@@ -451,7 +451,7 @@ void WT_VoiceFilter (S_FILTER_CONTROL *pFilter, S_WT_INT_FRAME *pWTIntFrame)
     numSamples = pWTIntFrame->numSamples;
     if (numSamples <= 0) {
         ALOGE("b/26366256");
-        android_errorWriteLog(0x534e4554, "26366256");
+        //android_errorWriteLog(0x534e4554, "26366256");
         return;
     }
     pOutputBuffer = pWTIntFrame->pAudioBuffer;
@@ -599,7 +599,7 @@ void WT_InterpolateMono (S_WT_VOICE *pWTVoice, S_WT_INT_FRAME *pWTIntFrame)
     numSamples = pWTIntFrame->numSamples;
     if (numSamples <= 0) {
         ALOGE("b/26366256");
-        android_errorWriteLog(0x534e4554, "26366256");
+        //android_errorWriteLog(0x534e4554, "26366256");
         return;
     }
     pMixBuffer = pWTIntFrame->pMixBuffer;
